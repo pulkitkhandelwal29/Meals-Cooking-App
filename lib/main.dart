62e278234469ed36fef7b62476254dcdf1b81e5e
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-05-17 12:54:33 +0530

    Styling and Theming

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Meals Cooking',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodySmall: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              titleSmall: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold),
+            ),
       ),
       //Loads the widget on the first screen of app
       home: CategoriesScreen(),
