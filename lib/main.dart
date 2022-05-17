0eaedb75277203d79528757088a985d22d4f0087
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-05-17 16:51:48 +0530

    Adding TabBar to appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/tabs_screen.dart';
 import '../screens/meal_detail_screen.dart';
 import '../screens/category_meals_screen.dart';
 import '../screens/categories_screen.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       //Loads the widget on the first screen of app
-      home: CategoriesScreen(),
+      home: TabsScreen(),
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,27 +9,22 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //creating gridview and wrapping into scafold as it is now the main screen
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meals Cooking'),
-      ),
-      body: GridView(
-        padding: const EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES
-            .map(
-              (catData) => CategoryItem(
-                catData.id,
-                catData.title,
-                catData.color,
-              ),
-            )
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: const EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES
+          .map(
+            (catData) => CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color,
+            ),
+          )
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
