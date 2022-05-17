b2de380787a87c115eb04fe946ac7b9cbed18e1e
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-05-17 15:04:09 +0530

    Displaying recipe items and used network images

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../categories_data.dart';
+import '../widgets/category_item.dart';
+
+//This is the first screen appearing when opening an app
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //creating gridview and wrapping into scafold as it is now the main screen
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals Cooking'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.id,
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+import '../categories_data.dart';
+
+//recipes of the category when tapped
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // commented after named routers was implemented
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    //named routes arguments
+    final routeArgs =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    //filtering meals based on categories
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle!),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+              title: categoryMeals[index].title,
+              imageUrl: categoryMeals[index].imageUrl,
+              duration: categoryMeals[index].duration,
+              complexity: categoryMeals[index].complexity,
+              affordability: categoryMeals[index].affordability);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
